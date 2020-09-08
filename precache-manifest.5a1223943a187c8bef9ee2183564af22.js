6cbbbb472fbf0a748099d76b04a4be9ff7d4932a
Anjali_Srivastava <43489841+AnjaliSrivastava722@users.noreply.github.com>
2020-09-09 00:57:10 +0530

    Updates

---
+++ precache-manifest.5a1223943a187c8bef9ee2183564af22.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42140de6ac42452986eb8cb279622ace",
+    "url": "/Cache/index.html"
+  },
+  {
+    "revision": "55adb30b63bdecdab67c",
+    "url": "/Cache/static/css/main.1fde9f0a.chunk.css"
+  },
+  {
+    "revision": "76244000ba1b698b0514",
+    "url": "/Cache/static/js/2.811ef30a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Cache/static/js/2.811ef30a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55adb30b63bdecdab67c",
+    "url": "/Cache/static/js/main.092fe288.chunk.js"
+  },
+  {
+    "revision": "75096a48235f0e6f67d8",
+    "url": "/Cache/static/js/runtime-main.a70c85d4.js"
+  },
+  {
+    "revision": "5eba7207899fd7a29133b9543dd89af5",
+    "url": "/Cache/static/media/AI.5eba7207.jpeg"
+  },
+  {
+    "revision": "c8d156a79c499f0d56d775c480b063a7",
+    "url": "/Cache/static/media/C1.c8d156a7.jpeg"
+  },
+  {
+    "revision": "a0d1701a243eb5b995e13d099d2e0176",
+    "url": "/Cache/static/media/C2.a0d1701a.jpeg"
+  },
+  {
+    "revision": "f70176c4e73064d9098837f114dca0e5",
+    "url": "/Cache/static/media/C3.f70176c4.jpeg"
+  },
+  {
+    "revision": "f70176c4e73064d9098837f114dca0e5",
+    "url": "/Cache/static/media/CS.f70176c4.jpeg"
+  },
+  {
+    "revision": "f8afbb08fdc8c9a59665769a10e8da0d",
+    "url": "/Cache/static/media/CYBER_SECURITY.f8afbb08.jpeg"
+  },
+  {
+    "revision": "df658a4a734ab8a507e27afdee0a7a56",
+    "url": "/Cache/static/media/DS_ALGO.df658a4a.jpeg"
+  },
+  {
+    "revision": "8cc1dc24c95e731fefecda9df69962da",
+    "url": "/Cache/static/media/ML.8cc1dc24.jpeg"
+  },
+  {
+    "revision": "f7223ca8e66500cf803699016d0b7431",
+    "url": "/Cache/static/media/WEB_DEV.f7223ca8.jpeg"
+  },
+  {
+    "revision": "f458f9bcc895dd3cadb4acdc96784716",
+    "url": "/Cache/static/media/art.f458f9bc.png"
+  }
+]);

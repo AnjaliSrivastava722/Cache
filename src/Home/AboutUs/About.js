b12b3407897e83079ece006eb564543bb300c59a
Anjali_Srivastava <43489841+AnjaliSrivastava722@users.noreply.github.com>
2020-09-08 22:53:26 +0530

    Testing heroku

--- src/Home/AboutUs/About.js
+++ src/Home/AboutUs/About.js
@@ -32,7 +32,7 @@ function About() {
                   <Grid item xs={12}  sm={8} style={{textAlign:"center"}}>
                       <Typography>
                           <h1 className={classes.styles}>About Us</h1>
-                          <h4 className={classes.style2}>We are a group of IIT IIM alumni with +15 years of 
+                          <h4 className={classes.style2}>We are stupid ass a group of IIT IIM alumni with +15 years of 
                               combined  work experience across global software comapnies,
                               investment bank and next-gen(unicorn) startups across EdTech,FoodTech
                               and FinTech.
